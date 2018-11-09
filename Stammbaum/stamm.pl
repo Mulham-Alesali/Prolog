85079aa516f7f65a6706b474f73e0944c82ab807
Mulham-Alesali <MB3.EMAIL@GMAIL.COM>
2018-11-09 22:49:46 +0100

    Create Stamm.pl

---
+++ Stammbaum/stamm.pl
@@ -0,0 +1,53 @@
+% -------------------------------------------------------
+% 
+% Stammbaum
+% 
+% -------------------------------------------------------
+ 
+maennlich(robert).
+maennlich(stephen).
+maennlich(henry).
+maennlich(peter).
+maennlich(john).
+maennlich(george).
+maennlich(richard).
+maennlich(james).
+
+weiblich(mary).
+weiblich(donna).
+weiblich(pamela).
+weiblich(julia).
+weiblich(elizabeth).
+weiblich(roberta).
+weiblich(susan).
+weiblich(harriet).
+weiblich(catherine).
+weiblich(melissa).
+
+elternteil(john,donna).
+elternteil(john,mary).
+elternteil(john,george).
+elternteil(catherine, donna).
+elternteil(catherine,mary).
+elternteil(catherine,george).
+elternteil(pamela,james).
+elternteil(george,james).
+
+elternteil(mary,julia).
+elternteil(mary,peter).
+elternteil(richard,julia).
+elternteil(richard,peter).
+
+elternteil(robert,stephen).
+elternteil(robert,henry).
+elternteil(elizabeth,stephen).
+elternteil(elizabeth,henry).
+
+elternteil(henry,harriet).
+elternteil(henry,susan).
+elternteil(roberta,harriet).
+elternteil(roberta,susan).
+
+elternteil(peter,melissa).
+elternteil(harriet,melissa).
+
